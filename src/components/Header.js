e2a3f838196798662bf0baeabfd11a99114e2c07
jaidernunes <jaidernf@gmail.com>
2022-11-23 19:37:05 -0300

    Req 4: Bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,14 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 export default function Header() {
-  const { omniFilter } = useContext(PlanetsContext);
+  const { setNameFilter,
+    setNumericFilter, numericFilter,
+    setColumnFilter, columnFilter,
+    setComparisonFilter, comparisonFilter,
+    setValueFilter, valueFilter } = useContext(PlanetsContext);
+  // const [columnFilter, setColumnFilter] = useState('population');
+  // const [comparisonFilter, setComparisonFilter] = useState('>');
+  // const [valueFilter, setValueFilter] = useState(null);
 
   return (
     <>
@@ -13,9 +20,64 @@ export default function Header() {
           data-testid="name-filter"
           name="nameFilter"
           id="name"
-          onChange={ omniFilter }
+          onChange={ ({ target }) => setNameFilter(target.value) }
         />
+        <br />
+
+        <select
+          data-testid="column-filter"
+          name="columnFilter"
+          id="columnFilter"
+          value={ columnFilter }
+          onChange={ ({ target }) => setColumnFilter(target.value) }
+        >
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
+        </select>
+
+        <select
+          name="comparisonFilter"
+          id="comparisonFilter"
+          data-testid="comparison-filter"
+          value={ comparisonFilter }
+          onChange={ ({ target }) => setComparisonFilter(target.value) }
+        >
+          <option>maior que</option>
+          <option>menor que</option>
+          <option>igual a</option>
+        </select>
+
+        <input
+          type="number"
+          name="valueFilter"
+          id="valueFilter"
+          data-testid="value-filter"
+          value={ valueFilter }
+          onChange={ ({ target }) => setValueFilter(target.value) }
+        />
+
+        <button
+          data-testid="button-filter"
+          type="button"
+          onClick={ () => {
+            setNumericFilter([...numericFilter,
+              { columnFilter, comparisonFilter, valueFilter }]);
+            console.log(numericFilter);
+          } }
+          // disabled={ !valueFilter }
+        >
+          FILTRAR
+        </button>
       </form>
+      <div>
+        {numericFilter.map((filter, i) => (
+          <div key={ `filter${i}` }>
+            {`${filter.columnFilter} ${filter.comparisonFilter} ${filter.valueFilter}`}
+          </div>))}
+      </div>
     </>
 
   );
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -7,6 +7,11 @@ import PlanetsContext from './PlanetsContext';
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
+  const [nameFilter, setNameFilter] = useState('');
+  const [columnFilter, setColumnFilter] = useState('population');
+  const [comparisonFilter, setComparisonFilter] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState('0');
+  const [numericFilter, setNumericFilter] = useState([]);
 
   useEffect(() => {
     const getAPI = async () => {
@@ -23,44 +28,80 @@ function PlanetsProvider({ children }) {
     };
 
     getAPI();
-    // [setData]
   }, []);
 
-  // const omniFilter = useCallback(
-  //   ({ target: { id, value } }) => (filteredData.length > 0
-  //     ? setFilteredData(filteredData.filter((planet) => planet[id].includes(value)))
-  //     : setFilteredData(data.filter((planet) => planet[id].includes(value)))),
-  //   [filteredData, data],
-  // );
-
-  const omniFilter = ({ target: { id, value } }) => (
-    setFilteredData(data.filter((planet) => planet[id].includes(value))));
-  // const omniFilter = ({ target: { id, value } }) => (filteredData.length > 0
-  //   ? setFilteredData(filteredData.filter((planet) => planet[id].includes(value)))
-  //   : setFilteredData(data.filter((planet) => planet[id].includes(value))));
-
-  // [filteredData, data],
-
-  //     ? setFilteredData(filteredData.filter((planet) => planet[type].includes(value)))
-  //     : setFilteredData(data.filter((planet) => planet[type].includes(value))));
-  // }, [filteredData, data]);
-  // const filterName = ({ value }) => {
-  //   const fiteredByName = data.filter((planet) => planet.name.includes(value));
+  // const omniFilter = ({ target: { id, value } }) => (
+  //   setFilteredData(data.filter((planet) => planet[id].includes(value))));
+  // const eachNumFilter = () => {
+  //   let tempData = filteredData;
+  //   numericFilter.forEach((numFilter) => {
+  //     tempData = tempData.filter((planet) => planet[numFilter.columnFilter]
+  //   > numFilter.valueFilter);
+  //   });
+  //   setFilteredData(tempData);
   // };
-
   // solução por Gabriela Ventura. Esclarecido na monitoria da manhã com Aline. 23/11
   // const dataMemo = useMemo(() => ({
   //   data, omniFilter,
   // }), [data, omniFilter]);
 
-  const value = {
-    omniFilter,
+  // columnFilter, comparisonFilter, valueFilter
+
+  useEffect(() => {
+    const eachNumFilter = () => {
+      let tempData = filteredData;
+      console.log(tempData);
+
+      tempData = tempData.filter((planet) => planet.name.includes(nameFilter));
+
+      numericFilter.forEach((numFilter) => {
+        console.log(numFilter.comparisonFilter);
+        console.log(numFilter.comparisonFilter);
+        if (numFilter.comparisonFilter === 'maior que') {
+          tempData = tempData.filter(
+            (planet) => planet[numFilter.columnFilter] > numFilter.valueFilter
+            && planet[numFilter.columnFilter] !== 'unknown',
+          );
+        }
+        if (numFilter.comparisonFilter === 'menor que') {
+          tempData = tempData.filter(
+            (planet) => planet[numFilter.columnFilter] < numFilter.valueFilter
+            && planet[numFilter.columnFilter] !== 'unknown',
+          );
+        }
+        if (numFilter.comparisonFilter === 'igual a') {
+          tempData = tempData.filter(
+            (planet) => planet[numFilter.columnFilter] === numFilter.valueFilter
+            && planet[numFilter.columnFilter] !== 'unknown',
+          );
+        }
+      });
+
+      setFilteredData(tempData);
+      console.log(tempData);
+    };
+    eachNumFilter();
+  }, [nameFilter, numericFilter]);
+
+  const providerProps = {
+    // omniFilter,
     data,
     filteredData,
+    nameFilter,
+    setNameFilter,
+    numericFilter,
+    setColumnFilter,
+    columnFilter,
+    setComparisonFilter,
+    comparisonFilter,
+    setValueFilter,
+    valueFilter,
+    setNumericFilter,
+    // eachNumFilter,
   };
 
   return (
-    <PlanetsContext.Provider value={ value }>
+    <PlanetsContext.Provider value={ providerProps }>
       {children}
     </PlanetsContext.Provider>
   );
